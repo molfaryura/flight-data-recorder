ddfa56abf3c391505f480006c2fdf60367727bab
molfaryura <oleksandrr1996@protonmail.com>
2026-06-17 11:38:22 +0000

    Update parse, add functions

--- recorder.c
+++ recorder.c
@@ -2,11 +2,12 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "recorder.h"
 
 
 
-void parse(char *buffer, int count, Packet *packets){
+int parse(char *buffer, int count, Packet **packets, char* time){
 
     const char delimeters[] = "$,#";
 
@@ -16,14 +17,15 @@ void parse(char *buffer, int count, Packet *packets){
 
     if (str == NULL) {
             printf("Помилка: Отримано порожній або некоректний пакет!\n");
-            return;
+            return 1;
         }
 
-    if(count > 0 ){
-        packets = realloc(packets, count * sizeof(Packet));
-    }
 
-    strcpy(packets[count].telemetry, str);
+    *packets = realloc(*packets, (count + 1) * sizeof(Packet));
+
+
+    strcpy((*packets)[count].time, time);
+    strcpy((*packets)[count].telemetry, str);
 
     int i = 0;
 
@@ -32,17 +34,50 @@ void parse(char *buffer, int count, Packet *packets){
         int value = atoi(str);
 
         if(i == 0){
-            packets[count].speed = (uint8_t)value;
+            (*packets)[count].speed = (uint8_t)value;
         }
         else if(i == 1){
-            packets[count].battery = (uint8_t)value;
+            (*packets)[count].satellites = (uint8_t)value;
+
         }
         else{
-            packets[count].satellites = (uint8_t)value;
+            (*packets)[count].battery = (uint8_t)value;
+            if(value <= 15){
+                return 1;
+            }
         }
 
         i++;
 
+    }
+    return 0;
+
+}
+
+
+void write_to_file(Packet *packets, int count){
+
+
+    FILE *file = fopen("blackbox.bin", "wb");
+
+    fwrite(packets, sizeof(Packet), count, file);
+
+
+    fclose(file);
+}
+
+void read_from_file(int count){
+
+    FILE *file = fopen("blackbox.bin", "rb");
+
+    Packet packets[count];
+
+    fread(packets, sizeof(Packet), count, file);
+
+    fclose(file);
+
+    for (int i = 0; i < count; i++) {
+        printf("%s,%s,%d,%d,%d\n", packets[i].time, packets[i].telemetry, packets[i].speed, packets[i].satellites, packets[i].battery);
     }
 
 }
