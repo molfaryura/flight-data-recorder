3c6093753e87d5b22928f78f86384399be1a5928
molfaryura <oleksandrr1996@protonmail.com>
2026-06-25 05:34:30 +0000

    Update Packet

--- recorder.h
+++ recorder.h
@@ -3,15 +3,19 @@
 
 #include <stdint.h>
 
-typedef struct{
-    char time[20];
-    char telemetry[20];
-    uint8_t speed;
-    uint8_t battery;
-    uint8_t satellites;
+typedef union{
+    uint64_t raw;
+    struct{
+        uint32_t time;
+        uint8_t telemetry;
+        uint8_t speed;
+        uint8_t battery: 7;
+        unsigned int satellites: 5;
+    };
+
 } Packet;
 
-int parse(char *buffer, int count, Packet **packets, char* time);
+int parse(char *buffer, int count, Packet **packets, uint32_t time);
 void write_to_file(char file_name[], Packet *packets, int count);
 void* read_from_file(char file_name[]);
 
